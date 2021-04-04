4fe8a92097eabda6a713fc82c5618c0c0e19f9f7
Remy Govaerts <rem.govaerts@gmail.com>
2021-04-04 16:52:01 +0200

    set errors to JSON

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :tackle_box_cds, TackleBoxCds.Repo,
 # with webpack to recompile .js and .css sources.
 config :tackle_box_cds, TackleBoxCdsWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
